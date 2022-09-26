9acad70bccc49a48f648f227b8312c39682d7f78
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-09-26 12:13:32 -0500

    First commit

---
+++ app/src/main/java/com/mediapros/socialmed/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mediapros.socialmed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
