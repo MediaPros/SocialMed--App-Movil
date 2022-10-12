7f19c6888906b322c58a6a8686c2ee0a206316e3
mauriprado <104120014+mauriprado@users.noreply.github.com>
2022-10-12 14:35:16 -0500

    feat(menu): Added menu to home activity.

--- app/src/main/java/com/mediapros/socialmed/home/controller/activities/HomeActivity.kt
+++ app/src/main/java/com/mediapros/socialmed/home/controller/activities/HomeActivity.kt
@@ -1,16 +1,21 @@
 package com.mediapros.socialmed.home.controller.activities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import androidx.annotation.NonNull
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mediapros.socialmed.R
 import com.mediapros.socialmed.RetrofitBuilder
 import com.mediapros.socialmed.StateManager
 import com.mediapros.socialmed.errors.controller.activities.ReportErrorsActivity
 import com.mediapros.socialmed.home.adapter.RecommendedDoctorAdapter
+import com.mediapros.socialmed.security.controller.activities.RegisterActivity
 import com.mediapros.socialmed.security.models.User
 import com.mediapros.socialmed.security.network.UserService
 import retrofit2.Call
@@ -24,15 +29,25 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        rvRecommendedDoctors = findViewById(R.id.rvRecommendedDoctors)
 
         loadRecommendedDoctors()
+        val bnvMenu = findViewById<BottomNavigationView>(R.id.bnvMenu)
+        bnvMenu.selectedItemId = R.id.menu_errors
+        
 
-        rvRecommendedDoctors = findViewById(R.id.rvRecommendedDoctors)
         val btReportError = findViewById<Button>(R.id.btReportError)
+
+
 
         btReportError.setOnClickListener {
             goToReportErrorActivity()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_errors) goToReportErrorActivity()
+        return super.onOptionsItemSelected(item)
     }
 
     private fun goToReportErrorActivity() {
